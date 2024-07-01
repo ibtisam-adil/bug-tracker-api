e0aa210241a7ab72036e349167901954f7d15caa
ibtisam-adil <ibtisamadil273@gmail.com>
2024-07-01 11:30:50 +0500

    cors error

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins "https://bug-zilla.netlify.app", "https://bug-zill.netlify.app", "http://localhost:5173", "https://bgzz.netlify.app"
+      origins "https://bug-zilla.netlify.app", "https://bug-zill.netlify.app", "http://localhost:5173", "https://bgzz.netlify.app", "http://localhost:5174",
   
       resource "*",
         headers: :any,
